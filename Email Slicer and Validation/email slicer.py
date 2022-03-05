542078fe9ffc7b3956bf5bf69cd4dce15f59e283
Navya <vaddinavya.g@gmail.com>
2022-03-05 19:19:46 +0530

    initial commit

--- Email Slicer and Validation/email slicer.py
+++ Email Slicer and Validation/email slicer.py
@@ -1,5 +1,7 @@
+# Ask user to enter any email and remove any unnecessary white spaces
 email = input("Enter Your Email: ").strip()
 
+#Get the index of @
 username = email[:email.index('@')]
 domain = email[email.index('@') + 1:]
 
